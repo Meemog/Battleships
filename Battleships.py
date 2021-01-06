8e65a8eb3ce6f11785b408fd4ea7186e8e8a6324
Meemog <36992704+Meemog@users.noreply.github.com>
2021-01-06 09:13:37 +0000

    Update Battleships.py

--- Battleships.py
+++ Battleships.py
@@ -1,6 +1,6 @@
 class Ship:
-    def __init__(self, shipType, length, timesHit):
+    def __init__(self, shipType, length, timesHit, isDestroyed = False):
         self.shipType = shipType
         self.length = length
         self.timesHit = timesHit
-        self.isDestroyed = True
+        self.isDestroyed = isDestroyed
