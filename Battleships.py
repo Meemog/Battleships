cf5fa51dc41d737acd7d3d35c019f2de935032e0
Meemog <36992704+Meemog@users.noreply.github.com>
2021-01-06 11:01:57 +0000

    Added other types of ship

--- Battleships.py
+++ Battleships.py
@@ -4,3 +4,37 @@ class Ship:
         self.length = length
         self.timesHit = timesHit
         self.isDestroyed = isDestroyed
+
+    def GetShipType(self):
+        return self.shipType
+
+    def GetLength(self):
+        return self.length
+
+    def GetTimesHit(self):
+        return self.timesHit
+
+    def SetTimesHit(self, value):
+        self.timesHit = value
+    
+    def GetIsDestroyed(self):
+        return self.isDestroyed
+    
+    def SetIsDestroyed(self, value):
+        self.isDestroyed = value
+
+class Submarine(Ship):
+    def __init__(self, timesHit):
+        super().__init__("Submarine", 2, timesHit)
+
+class Battleship(Ship):
+    def __init__(self, timesHit):
+        super().__init__("Battleship", 3, timesHit)
+
+class Destroyer(Ship):
+    def __init__(self, timesHit):
+        super().__init__("Destroyer", 4, timesHit)
+
+class AircraftCarrier(Ship):
+    def __init__(self, timesHit):
+        super().__init__("Aircraft Carrier", 5, timesHit)
